ea518c4cfb6380457bde3727a3e5ab315c3d18f6
laraval <manish3xdizital@gmail.com>
2024-01-17 17:46:06 +0530

    1st Demo

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
 Route::view('/', 'home');
 Route::view('/about-us', 'about');
 Route::view('/services', 'services');
